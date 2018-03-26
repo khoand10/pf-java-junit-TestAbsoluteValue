da3be3ff392b74904deac27e84fb26f2944938a4
khoa developer <khoandph05225@fpt.edu.vn>
2018-03-26 11:32:24 +0700

    commit

---
+++ src/AbsoluteValue.java
@@ -0,0 +1,9 @@
+package main;
+
+public class AbsoluteValue {
+    public static int findAbsolute(int number){
+        if(number < 0)
+            return  -number;
+        return number;
+    }
+}
